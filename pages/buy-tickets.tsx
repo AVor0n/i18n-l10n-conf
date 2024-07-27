8cf784c1b669dc9486253db05b7284a8b9d2070e
Andrey Voronin <AVor0n@yandex.ru>
2024-07-27 16:06:18 +0300

    localize ticket page

--- pages/buy-tickets.tsx
+++ pages/buy-tickets.tsx
@@ -1,3 +1,24 @@
 import { BuyTicketsPage } from "@/pages/buy-tickets";
+import { getUserLocale } from '@/shared/getLocale';
+import type { NextApiRequest } from 'next/types';
 
 export default BuyTicketsPage;
+
+export async function getServerSideProps({
+    req,
+    locale: localeFromRouter,
+}: {
+    req: NextApiRequest;
+    locale: string;
+}) {
+    const locale = localeFromRouter || getUserLocale(req);
+    const messagesModule = await import(`../src/pages/buy-tickets/locale/${locale}.json`);
+    const messages = messagesModule.default;
+
+    return {
+        props: {
+            locale,
+            messages,
+        },
+    };
+}
--- src/pages/buy-tickets/index.tsx
+++ src/pages/buy-tickets/index.tsx
@@ -1,46 +1,87 @@
-import classNames from "classnames/bind";
-import { NextPage } from "next";
-import Head from "next/head";
-import { useState } from "react";
+import classNames from 'classnames/bind';
+import Head from 'next/head';
+import { useState } from 'react';
 
-import { Button } from "@/components/button";
-import { HorizontalDivider } from "@/components/horizontal-divider";
-import { PageHeader } from "@/components/page-header";
-import { Ticket } from "@/components/ticket";
+import { Button } from '@/components/button';
+import { HorizontalDivider } from '@/components/horizontal-divider';
+import { PageHeader } from '@/components/page-header';
+import { Ticket } from '@/components/ticket';
 
-import styles from "./styles.module.css";
+import styles from './styles.module.css';
+import { FormattedMessage, IntlProvider, createIntl, createIntlCache } from 'react-intl';
 
 const cx = classNames.bind(styles);
 
-export const BuyTicketsPage: NextPage = () => {
+export const BuyTicketsPage = ({
+    locale,
+    messages,
+}: {
+    locale: string;
+    messages: Record<string, string>;
+}) => {
+    const cache = createIntlCache();
+    const intl = createIntl({ locale, messages }, cache);
+
     const [selectedPrice, setSelectedPrice] = useState(2500);
     const [ticketCount, setTicketCount] = useState(1);
 
     const totalPrice = selectedPrice * ticketCount;
 
     return (
-        <>
+        <IntlProvider locale={locale} messages={messages}>
             <Head>
-                <title>Купить билеты - I&L–2024</title>
+                <title>
+                    {intl.formatMessage(
+                        {
+                            id: 'buyTicketsPage.title',
+                            defaultMessage: 'Купить билеты - I&L–{year}',
+                        },
+                        {
+                            year: intl.formatDate(new Date(), { year: 'numeric' }),
+                        },
+                    )}
+                </title>
             </Head>
 
-            <PageHeader title="Купить билеты" />
+            <PageHeader
+                title={intl.formatMessage({
+                    id: 'header.buyTickets',
+                    defaultMessage: 'Купить билеты',
+                })}
+            />
 
-            <form className={cx("form")}>
+            <form className={cx('form')}>
                 <HorizontalDivider />
 
-                <fieldset className={cx("form__tickets")}>
-                    <div className={cx("form__label")}>Выберите тип билета</div>
-                    <div className={cx("form__tickets-wrapper")}>
+                <fieldset className={cx('form__tickets')}>
+                    <div className={cx('form__label')}>
+                        <FormattedMessage
+                            id="buyTicketsPage.chooseTicketType"
+                            defaultMessage={'Выберите тип билета'}
+                        />
+                    </div>
+                    <div className={cx('form__tickets-wrapper')}>
                         <Ticket
                             id="ticket-default"
                             title={
-                                <>
-                                    Входной билет <br /> 2 500,00 ₽
-                                </>
+                                <FormattedMessage
+                                    id="buyTicketsPage.defaultTicket"
+                                    defaultMessage={'Входной билет {br} {price}'}
+                                    values={{
+                                        price: intl.formatNumber(2500, {
+                                            style: 'currency',
+                                            currency: 'RUB',
+                                        }),
+                                        br: <br />,
+                                    }}
+                                />
                             }
                             price={2500}
-                            description="Простой входной билет предоставляет доступ на мероприятие и является вашим пропуском на конференцию"
+                            description={intl.formatMessage({
+                                id: 'buyTicketsPage.defaultTicketDescription',
+                                defaultMessage:
+                                    'Простой входной билет предоставляет доступ на мероприятие и является вашим пропуском на конференцию',
+                            })}
                             defaultChecked
                             onChange={setSelectedPrice}
                         />
@@ -49,68 +90,122 @@ export const BuyTicketsPage: NextPage = () => {
                             vip
                             id="ticket-default"
                             title={
-                                <>
-                                    VIP посещение <br /> 15 000,00 ₽
-                                </>
+                                <FormattedMessage
+                                    id="buyTicketsPage.vipTicket"
+                                    defaultMessage={'VIP посещение {br} {price}'}
+                                    values={{
+                                        price: intl.formatNumber(15000, {
+                                            style: 'currency',
+                                            currency: 'RUB',
+                                        }),
+                                        br: <br />,
+                                    }}
+                                />
                             }
                             price={15000}
-                            description="Получите дополнительный доступ к закрытым дискуссиям, личным встречам с ведущими экспертами"
+                            description={intl.formatMessage({
+                                id: 'buyTicketsPage.vipTicketDescription',
+                                defaultMessage:
+                                    'Получите дополнительный доступ к закрытым дискуссиям, личным встречам с ведущими экспертами',
+                            })}
                             onChange={setSelectedPrice}
                         />
                     </div>
                 </fieldset>
 
-                <fieldset className={cx("form__inputs")}>
-                    <label className={cx("form__inputs-label")}>
-                        <div className={cx("form__label")}>Имя и фамилия</div>
+                <fieldset className={cx('form__inputs')}>
+                    <label className={cx('form__inputs-label')}>
+                        <div className={cx('form__label')}>
+                            <FormattedMessage
+                                id="buyTicketsPage.name"
+                                defaultMessage={'Имя и фамилия'}
+                            />
+                        </div>
                         <input
-                            className={cx("form__input")}
+                            className={cx('form__input')}
                             type="text"
-                            placeholder="Введите имя и фамилию"
+                            placeholder={intl.formatMessage({
+                                id: 'buyTicketsPage.namePlaceholder',
+                                defaultMessage: 'Введите имя и фамилию',
+                            })}
                         />
                     </label>
 
-                    <label className={cx("form__inputs-label")}>
-                        <div className={cx("form__label")}>Электронная почта</div>
+                    <label className={cx('form__inputs-label')}>
+                        <div className={cx('form__label')}>
+                            {intl.formatMessage({
+                                id: 'buyTicketsPage.email',
+                                defaultMessage: 'Электронная почта',
+                            })}
+                        </div>
                         <input
-                            className={cx("form__input")}
+                            className={cx('form__input')}
                             type="email"
                             placeholder="example@example.com"
                         />
                     </label>
 
-                    <label className={cx("form__inputs-label")}>
-                        <div className={cx("form__label")}>Номер телефона</div>
-                        <input className={cx("form__input", "form__input_phone")} type="tel" />
+                    <label className={cx('form__inputs-label')}>
+                        <div className={cx('form__label')}>
+                            {intl.formatMessage({
+                                id: 'buyTicketsPage.phoneNumber',
+                                defaultMessage: 'Номер телефона',
+                            })}
+                        </div>
+                        <input className={cx('form__input', 'form__input_phone')} type="tel" />
                     </label>
 
-                    <div className={cx("form__inputs-label")}>
-                        <div className={cx("form__label")}>Количество билетов</div>
+                    <div className={cx('form__inputs-label')}>
+                        <div className={cx('form__label')}>
+                            {intl.formatMessage({
+                                id: 'buyTicketsPage.ticketCount',
+                                defaultMessage: 'Количество билетов',
+                            })}
+                        </div>
                         <div>
-                            {[1, 2, 3, 4, 5, 6].map((count) => (
-                                <div className={cx("form__tickets-count")} key={count}>
+                            {[1, 2, 3, 4, 5, 6].map(count => (
+                                <div className={cx('form__tickets-count')} key={count}>
                                     <input
-                                        className={cx("form__tickets-count-input")}
+                                        className={cx('form__tickets-count-input')}
                                         type="radio"
                                         name="tickets"
                                         id={`tickets_${count}`}
                                         checked={count === ticketCount}
                                         onChange={() => setTicketCount(count)}
                                     />
-                                    <label htmlFor={`tickets_${count}`}>{count}</label>
+                                    <label htmlFor={`tickets_${count}`}>
+                                        {intl.formatNumber(count)}
+                                    </label>
                                 </div>
                             ))}
                         </div>
                     </div>
                 </fieldset>
 
-                <div className={cx("form__submit")}>
-                    <Button type="button">Оплатить {totalPrice} ₽</Button>
-                    <div className={cx("form__submit-description")}>
-                        Нажимая «Оплатить», вы соглашаетесь с условиями приобретения и офертой
+                <div className={cx('form__submit')}>
+                    <Button type="button">
+                        {intl.formatMessage(
+                            {
+                                id: 'buyTicketsPage.pay',
+                                defaultMessage: 'Оплатить {totalPrice}',
+                            },
+                            {
+                                totalPrice: intl.formatNumber(totalPrice, {
+                                    style: 'currency',
+                                    currency: 'RUB',
+                                }),
+                            },
+                        )}
+                    </Button>
+                    <div className={cx('form__submit-description')}>
+                        {intl.formatMessage({
+                            id: 'buyTicketsPage.agreement',
+                            defaultMessage:
+                                'Нажимая «Оплатить», вы соглашаетесь с условиями приобретения и офертой',
+                        })}
                     </div>
                 </div>
             </form>
-        </>
+        </IntlProvider>
     );
 };
